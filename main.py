153447bbfffbddabd7af3f81ef4dbe08c36e3283
Mathias <mathias.nichele@mytapp.com.br>
2023-03-28 20:59:21 -0300

    Mudando forma da lista

--- main.py
+++ main.py
@@ -1,80 +1,80 @@
 import random
-import numpy as np
+import copy
 
 # Definindo meu objetivo
-obj = np.reshape([1, 2, 3, 4, 5, 6, 7, 8, ""], (3,3))
+obj = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 print("Meu objetivo é:")
 print(obj)
 
-# x = list(range(1,9)) # Criar uma lista com números entre 1 e 8
+x = list(range(0,9)) # Criar uma lista com números entre 1 e 8
+random.shuffle(x) # Embaralhando a lista
 
-# x.append("") # Adicionando valor vázio
+x1 = x[:3]
+x2 = x[3:6]
+x3 = x[6:]
 
-# random.shuffle(x) # Embaralhando a lista
-x = ["", 1, 2, 4, 5, 3, 7, 8, 6]
+x = [x1, x2, x3]
+
+# x = [[0, 1, 2], [4, 5, 3], [7, 8, 6]]
+# x = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
 # Criando Matriz a partir da lista
-estadoInicial = np.reshape(x, (3,3))
-estadoAtual = estadoInicial
-estadoAnterior = []
+estadoAtual = x.copy()
 print("Matriz de estado inicial é:")
 print(estadoAtual)
 
 visitados = [] # Estados conhecidos
 abertos = [estadoAtual]
 
-# Verificando se o estado é meu objetivo
-def verificaObj(m):
-    i=0
-    for l in range(0,3):
-        for c in range(0,3):
-            if m[l][c] != obj[l][c]:
-                return i
-            else:
-                i = i+1
-                if i == 9:
-                    return i
-
-# Buscando posição vázia na matriz
-def buscaEstados(m):
+def buscaVazio(m):
     abertos = []
     for l in range(0,3):
         for c in range(0,3):
-            copy = m.copy()
-            if l < 2 and m[l+1][c] == "":
-                copy[l+1][c] = copy[l][c]
-                copy[l][c] = ""
-                abertos.append(copy)
-            elif c < 2 and m[l][c+1] == "":
-                copy[l][c+1] = copy[l][c]
-                copy[l][c] = ""
-                abertos.append(copy)
-            elif l > 0 and m[l-1][c] == "":
-                copy[l-1][c] = copy[l][c]
-                copy[l][c] = ""
-                abertos.append(copy)
-            elif c > 0 and m[l][c-1] == "":
-                copy[l][c-1] = copy[l][c]
-                copy[l][c] = ""
-                abertos.append(copy)
+            if m[l][c] == 0:
+                return l, c
+
+def buscaEstados(m, l, c):
+    abertos = []
+
+    if l < 2:
+        temp = copy.deepcopy(m)
+        temp[l][c] = temp[l+1][c]
+        temp[l+1][c] = 0
+        abertos.append(temp)
+    if c < 2:
+        temp = copy.deepcopy(m)
+        temp[l][c] = temp[l][c+1]
+        temp[l][c+1] = 0
+        abertos.append(temp)
+    if l > 0:
+        temp = copy.deepcopy(m)
+        temp[l][c] = temp[l-1][c]
+        temp[l-1][c] = 0
+        abertos.append(temp)
+    if c > 0:
+        temp = copy.deepcopy(m)
+        temp[l][c] = temp[l][c-1]
+        temp[l][c-1] = 0
+        abertos.append(temp)
+
     return abertos
 
+print("\n")
+
+i = 0
 # -----> Busca em Amplitude = Custo Uniforme com custo unitário <-----
 while 1:
     state = abertos.pop(0)
-    if verificaObj(state) == 9:
+    i=i+1
+    
+    if state == obj:
         print("Parabéns você chegou no objetivo!!!")
-        print(state)
+        print(state, "em",i, "tentativas \n")
         break
-    else: # Se o estadoAtual não solução
-        for i in range(len(abertos)):
-            if np.all(abertos[i] == state):
-                abertos.pop(i)
-                break
+    else:
         visitados.append(state)
-
-        a = buscaEstados(state)
+        l, c = buscaVazio(state)
+        a = buscaEstados(state, l, c)
         for elemento in a:
-            if not any((elemento == v).all() for v in visitados):
+            if elemento not in visitados:
                 abertos.append(elemento)
-                for i in abertos:
-                    print(i)
