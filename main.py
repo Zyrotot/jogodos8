02ef65fba358bec5199eccc7c585192c01a226d2
Mathias <mathias.nichele@mytapp.com.br>
2023-03-28 21:00:33 -0300

    Tirando comentarios

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import random
 import copy
 
-# Definindo meu objetivo
 obj = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 print("Meu objetivo é:")
 print(obj)
 
-x = list(range(0,9)) # Criar uma lista com números entre 1 e 8
-random.shuffle(x) # Embaralhando a lista
+x = list(range(0,9))
+random.shuffle(x)
 
 x1 = x[:3]
 x2 = x[3:6]
@@ -18,12 +17,11 @@ x = [x1, x2, x3]
 # x = [[0, 1, 2], [4, 5, 3], [7, 8, 6]]
 # x = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 
-# Criando Matriz a partir da lista
 estadoAtual = x.copy()
 print("Matriz de estado inicial é:")
 print(estadoAtual)
 
-visitados = [] # Estados conhecidos
+visitados = []
 abertos = [estadoAtual]
 
 def buscaVazio(m):
@@ -59,10 +57,7 @@ def buscaEstados(m, l, c):
 
     return abertos
 
-print("\n")
-
 i = 0
-# -----> Busca em Amplitude = Custo Uniforme com custo unitário <-----
 while 1:
     state = abertos.pop(0)
     i=i+1
