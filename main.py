96addccf2f53748c7f1f5cf1e73065aa9a80567b
Mathias <mathias.nichele@mytapp.com.br>
2023-03-28 21:10:29 -0300

    Melhorando

--- main.py
+++ main.py
@@ -1,29 +1,6 @@
 import random
 import copy
 
-obj = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-print("Meu objetivo é:")
-print(obj)
-
-x = list(range(0,9))
-random.shuffle(x)
-
-x1 = x[:3]
-x2 = x[3:6]
-x3 = x[6:]
-
-x = [x1, x2, x3]
-
-# x = [[0, 1, 2], [4, 5, 3], [7, 8, 6]]
-# x = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-
-estadoAtual = x.copy()
-print("Matriz de estado inicial é:")
-print(estadoAtual)
-
-visitados = []
-abertos = [estadoAtual]
-
 def buscaVazio(m):
     abertos = []
     for l in range(0,3):
@@ -57,19 +34,63 @@ def buscaEstados(m, l, c):
 
     return abertos
 
-i = 0
-while 1:
-    state = abertos.pop(0)
-    i=i+1
-    
-    if state == obj:
-        print("Parabéns você chegou no objetivo!!!")
-        print(state, "em",i, "tentativas \n")
-        break
-    else:
+def buscaProfundidade():
+    i = 0
+    while 1:
+        state = abertos.pop(0)
+        i=i+1
+        
         visitados.append(state)
         l, c = buscaVazio(state)
         a = buscaEstados(state, l, c)
         for elemento in a:
             if elemento not in visitados:
+                if elemento == obj:
+                    print("\nObjetivo alcançado em",i, "tentativas!")
+                    print(state)
+                    return
                 abertos.append(elemento)
+
+def buscaAestrela():
+    i = 0
+    while 1:
+        state = abertos.pop(0)
+        i=i+1
+        
+        visitados.append(state)
+        l, c = buscaVazio(state)
+        a = buscaEstados(state, l, c)
+        for elemento in a:
+            if elemento not in visitados:
+                if elemento == obj:
+                    print("Objetivo alcançado em",i, "tentativas!\n")
+                    print(state)
+                    return
+                abertos.append(elemento)
+
+obj = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+print("Meu objetivo é:")
+print(obj)
+
+# x = list(range(0,9))
+# random.shuffle(x)
+
+# x1 = x[:3]
+# x2 = x[3:6]
+# x3 = x[6:]
+
+# x = [x1, x2, x3]
+
+x = [[0, 5, 3], [7, 2, 6], [4, 1, 8]]
+
+# x = [[0, 1, 2], [4, 5, 3], [7, 8, 6]]
+# x = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
+estadoAtual = x.copy()
+print("Matriz de estado inicial é:")
+print(estadoAtual)
+
+visitados = []
+abertos = [estadoAtual]
+
+buscaProfundidade()
